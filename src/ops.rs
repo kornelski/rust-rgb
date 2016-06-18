539bd8ea8dbd6825cbca6298d70e5e8a0fc39c82
Kornel Lesiński <kornel@geekhood.net>
2016-06-19 00:38:38 +0100

    Mul

--- src/ops.rs
+++ src/ops.rs
@@ -37,11 +37,29 @@ impl<T: Clone + Copy + Add> Add<T> for RGBA<T>
     }
 }
 
+impl<T: Clone + Copy + Mul> Mul<T> for RGB<T>
+    where T: Mul<Output=T> {
+    type Output = RGB<T>;
+    fn mul(self, r: T) -> Self::Output {
+        self.map(|l|l*r)
+    }
+}
+
+impl<T: Clone + Copy + Mul> Mul<T> for RGBA<T>
+    where T: Mul<Output=T> {
+    type Output = RGBA<T>;
+    fn mul(self, r: T) -> Self::Output {
+        self.map(|l|l*r)
+    }
+}
+
 #[test]
 fn test_math() {
     assert_eq!(RGB::new(2,4,6), RGB::new(1,2,3) + RGB{r:1,g:2,b:3});
     assert_eq!(RGB::new(2.,4.,6.), RGB::new(1.,3.,5.) + 1.);
+    assert_eq!(RGB::new(0.5,1.5,2.5), RGB::new(1.,3.,5.) * 0.5);
 
     assert_eq!(RGBA::new(2,4,6,8), RGBA::new(1,2,3,4) + RGBA{r:1,g:2,b:3,a:4});
     assert_eq!(RGBA::new(2i16,4,6,8), RGBA::new(1,3,5,7) + 1);
+    assert_eq!(RGBA::new(2,4,6,8), RGBA::new(1,2,3,4) * 2);
 }
