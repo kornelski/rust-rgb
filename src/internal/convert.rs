ee2857cc03ff516d43db7f6f542472f53da3b802
Kornel Lesiński <kornel@geekhood.net>
2016-08-11 18:14:37 +0100

    Drop opinionated conversions

--- src/internal/convert.rs
+++ src/internal/convert.rs
@@ -13,18 +13,14 @@ macro_rules! rgb_impl_from {
     }
 }
 
-rgb_impl_from!{RGB, u8,u16}
 rgb_impl_from!{RGB, u8,i16}
 rgb_impl_from!{RGB, u16,i32}
-rgb_impl_from!{RGB, u16,u32}
 
 rgb_impl_from!{RGB, u8,f32}
 rgb_impl_from!{RGB, u8,f64}
 rgb_impl_from!{RGB, u16,f32}
 rgb_impl_from!{RGB, u16,f64}
 
-rgb_impl_from!{RGB, i8,f32}
-rgb_impl_from!{RGB, i8,f64}
 rgb_impl_from!{RGB, i16,f32}
 rgb_impl_from!{RGB, i16,f64}
 
@@ -32,18 +28,13 @@ rgb_impl_from!{RGB, i32,f64}
 rgb_impl_from!{RGB, f32,f64}
 
 
-rgb_impl_from!{RGBA, u8,u16}
-rgb_impl_from!{RGBA, u8,i16}
 rgb_impl_from!{RGBA, u16,i32}
-rgb_impl_from!{RGBA, u16,u32}
 
 rgb_impl_from!{RGBA, u8,f32}
 rgb_impl_from!{RGBA, u8,f64}
 rgb_impl_from!{RGBA, u16,f32}
 rgb_impl_from!{RGBA, u16,f64}
 
-rgb_impl_from!{RGBA, i8,f32}
-rgb_impl_from!{RGBA, i8,f64}
 rgb_impl_from!{RGBA, i16,f32}
 rgb_impl_from!{RGBA, i16,f64}
 
