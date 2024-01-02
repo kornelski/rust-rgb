6dbe7c2d845d6ac4e6eb2276cab1db62fbbd1a26
Pro <twisted.fall@gmail.com>
2024-01-02 13:14:13 +0100

    Fix the from_over_into clippy warning
    https://rust-lang.github.io/rust-clippy/master/index.html#/from_over_into

--- src/internal/convert/tuple.rs
+++ src/internal/convert/tuple.rs
@@ -15,10 +15,10 @@ impl<T> From<(T,T,T)> for RGB<T> {
     }
 }
 
-impl<T> Into<(T,T,T)> for RGB<T> {
+impl<T> From<RGB<T>> for (T,T,T) {
     #[inline]
-    fn into(self) -> (T,T,T) {
-        (self.r, self.g, self.b)
+    fn from(value: RGB<T>) -> Self {
+        (value.r, value.g, value.b)
     }
 }
 
@@ -34,10 +34,10 @@ impl<T,A> From<(T,T,T,A)> for RGBA<T,A> {
     }
 }
 
-impl<T,A> Into<(T,T,T,A)> for RGBA<T,A> {
+impl<T,A> From<RGBA<T,A>> for (T,T,T,A) {
     #[inline]
-    fn into(self) -> (T,T,T,A) {
-        (self.r, self.g, self.b, self.a)
+    fn from(value: RGBA<T,A>) -> Self {
+        (value.r, value.g, value.b, value.a)
     }
 }
 
@@ -52,10 +52,10 @@ impl<T> From<(T,T,T)> for BGR<T> {
     }
 }
 
-impl<T> Into<(T,T,T)> for BGR<T> {
+impl<T> From<BGR<T>> for (T,T,T) {
     #[inline(always)]
-    fn into(self) -> (T,T,T) {
-        (self.b, self.g, self.r)
+    fn from(value: BGR<T>) -> Self {
+        (value.b, value.g, value.r)
     }
 }
 
@@ -71,10 +71,10 @@ impl<T,A> From<(T,T,T,A)> for BGRA<T,A> {
     }
 }
 
-impl<T,A> Into<(T,T,T,A)> for BGRA<T,A> {
+impl<T,A> From<BGRA<T,A>> for (T,T,T,A) {
     #[inline(always)]
-    fn into(self) -> (T,T,T,A) {
-        (self.b, self.g, self.r, self.a)
+    fn from(value: BGRA<T,A>) -> Self {
+        (value.b, value.g, value.r, value.a)
     }
 }
 
