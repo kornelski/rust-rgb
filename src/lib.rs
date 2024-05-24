a2d14524e724c870aa26b87dd01d9c3528f77a0e
ripytide <james.forsterer@gmail.com>
2024-05-24 17:29:06 +0100

    add all pixel type modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,17 @@
 #![doc(html_logo_url = "https://kornel.ski/rgb-logo.png")]
-#![no_std]
 #![warn(missing_docs)]
-
+#![no_std]
 // std is required to run unit tests
 #[cfg(test)]
 #[macro_use]
 extern crate std;
+
+mod abgr;
+mod argb;
+mod bgr;
+mod bgra;
+mod gray;
+mod gray_alpha;
+mod grb;
+mod rgb;
+mod rgba;
