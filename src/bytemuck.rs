33a34cfb5f58ff8b683b7ee0536a431f347b9e61
ripytide <james.forsterer@gmail.com>
2024-06-18 17:20:08 +0100

    remove bits from bytemuck macro

--- src/bytemuck.rs
+++ src/bytemuck.rs
@@ -1,18 +1,18 @@
 use crate::{Abgr, Argb, Bgr, Bgra, Gray, GrayA, Grb, Rgb, Rgba};
 
 macro_rules! bytemuck {
-    ($name:ident, [$($bit:tt),*]) => {
+    ($name:ident) => {
         unsafe impl<T> bytemuck::Zeroable for $name<T> where T: bytemuck::Zeroable {}
         unsafe impl<T> bytemuck::Pod for $name<T> where T: bytemuck::Pod {}
     };
 }
 
-bytemuck!(Rgb, [r, g, b]);
-bytemuck!(Bgr, [b, g, r]);
-bytemuck!(Grb, [g, r, b]);
-bytemuck!(Gray, [0]);
-bytemuck!(Rgba, [r, g, b, a]);
-bytemuck!(Argb, [a, r, g, b]);
-bytemuck!(Bgra, [b, g, r, a]);
-bytemuck!(Abgr, [a, b, g, r]);
-bytemuck!(GrayA, [0, 1]);
+bytemuck!(Rgb);
+bytemuck!(Bgr);
+bytemuck!(Grb);
+bytemuck!(Gray);
+bytemuck!(Rgba);
+bytemuck!(Argb);
+bytemuck!(Bgra);
+bytemuck!(Abgr);
+bytemuck!(GrayA);
