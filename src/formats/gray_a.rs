35280df43b41e63b13c269c9746ba7fe114a9890
ripytide <james.forsterer@gmail.com>
2024-06-19 11:43:42 +0100

    add optional defmt derives to pixel types

--- src/formats/gray_a.rs
+++ src/formats/gray_a.rs
@@ -1,5 +1,6 @@
 #[repr(C)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(feature = "defmt-03", derive(defmt::Format))]
 #[derive(Copy, Clone, Debug, Default, Eq, PartialEq, Ord, PartialOrd, Hash)]
 /// A `Grayscale + Alpha` pixel.
 pub struct GrayA<T, A = T>(
