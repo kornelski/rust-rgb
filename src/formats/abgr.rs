cc37465778841a9715da5f8ad3a1acd09754ce45
ripytide <james.forsterer@gmail.com>
2024-06-19 11:43:42 +0100

    add optional defmt derives to pixel types

--- src/formats/abgr.rs
+++ src/formats/abgr.rs
@@ -1,5 +1,6 @@
 #[repr(C)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(feature = "defmt-03", derive(defmt::Format))]
 #[derive(Copy, Clone, Debug, Default, Eq, PartialEq, Ord, PartialOrd, Hash)]
 /// An `ABGR` pixel.
 pub struct Abgr<T, A = T> {
