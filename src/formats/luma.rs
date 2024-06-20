77aa6493d6511e31f97f84143450e349843c3b5e
ripytide <james.forsterer@gmail.com>
2024-06-20 13:39:34 +0100

    re-word Luma docs to use the word `Brightness`

--- src/formats/luma.rs
+++ src/formats/luma.rs
@@ -2,10 +2,10 @@
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 #[cfg_attr(feature = "defmt-03", derive(defmt::Format))]
 #[derive(Copy, Clone, Debug, Default, Eq, PartialEq, Ord, PartialOrd, Hash)]
-/// A `Luminosity` pixel.
+/// A `Brightness` pixel.
 ///
 /// This pixel is commonly used for grayscale images.
 pub struct Luma<T> {
-    /// Luminosity Component
+    /// Brightness Component
     pub l: T,
 }
