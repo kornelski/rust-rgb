28243c1208d15a11f173457a88054838f78afd39
ripytide <james.forsterer@gmail.com>
2024-06-30 16:30:58 +0100

    refactor `ops` traits and genericize them on alpha type where applicable

--- src/legacy/internal/pixel.rs
+++ src/legacy/internal/pixel.rs
@@ -1,4 +1,3 @@
-
 /// Casting the struct to slices of its components
 pub trait ComponentSlice<T> {
     /// The components interpreted as an array, e.g. one `RGB` expands to 3 elements.
@@ -34,15 +33,16 @@ pub trait ComponentSlice<T> {
 ///
 /// Plain types are not allowed to contain struct padding, booleans, chars, enums, references or pointers.
 #[cfg(feature = "as-bytes")]
-pub trait ComponentBytes<T: crate::Pod> where Self: ComponentSlice<T> {
+pub trait ComponentBytes<T: crate::Pod>
+where
+    Self: ComponentSlice<T>,
+{
     /// The components interpreted as raw bytes, in machine's native endian. In `RGB` bytes of the red component are first.
     #[inline]
     fn as_bytes(&self) -> &[u8] {
         assert_ne!(0, core::mem::size_of::<T>());
         let slice = self.as_slice();
-        unsafe {
-            core::slice::from_raw_parts(slice.as_ptr().cast(), core::mem::size_of_val(slice))
-        }
+        unsafe { core::slice::from_raw_parts(slice.as_ptr().cast(), core::mem::size_of_val(slice)) }
     }
 
     /// The components interpreted as raw bytes, in machine's native endian. In `RGB` bytes of the red component are first.
@@ -51,7 +51,10 @@ pub trait ComponentBytes<T: crate::Pod> where Self: ComponentSlice<T> {
         assert_ne!(0, core::mem::size_of::<T>());
         let slice = self.as_mut_slice();
         unsafe {
-            core::slice::from_raw_parts_mut(slice.as_mut_ptr().cast(), core::mem::size_of_val(slice))
+            core::slice::from_raw_parts_mut(
+                slice.as_mut_ptr().cast(),
+                core::mem::size_of_val(slice),
+            )
         }
     }
 }
@@ -72,7 +75,8 @@ pub trait ComponentMap<DestPixel, SrcComponent, DestComponent> {
     ///
     /// Note that it returns the pixel directly, not an Interator.
     fn map<Callback>(&self, f: Callback) -> DestPixel
-        where Callback: FnMut(SrcComponent) -> DestComponent;
+    where
+        Callback: FnMut(SrcComponent) -> DestComponent;
 }
 
 /// Same as `ComponentMap`, but doesn't change the alpha channel (if there's any alpha).
@@ -81,5 +85,6 @@ pub trait ColorComponentMap<DestPixel, SrcComponent, DestComponent> {
     ///
     /// Note that it returns the pixel directly, not an Interator.
     fn map_c<Callback>(&self, f: Callback) -> DestPixel
-        where Callback: FnMut(SrcComponent) -> DestComponent;
+    where
+        Callback: FnMut(SrcComponent) -> DestComponent;
 }
