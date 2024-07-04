47223478bc295583eb20c0031b1f6de9cec5a356
ripytide <james.forsterer@gmail.com>
2024-07-04 17:31:42 +0100

    expand num traits definitions for het pixels

--- src/num_traits.rs
+++ src/num_traits.rs
@@ -2,7 +2,7 @@ use crate::{Abgr, Argb, Bgr, Bgra, Gray, GrayA, Grb, Rgb, Rgba, Rgbw};
 
 use num_traits::ops::checked::{CheckedAdd, CheckedDiv, CheckedMul, CheckedSub};
 
-macro_rules! num_traits {
+macro_rules! num_traits_without_alpha {
 ($name:ident, [$($bit:tt),*]) => {
     impl<T: CheckedAdd> CheckedAdd for $name<T> {
         #[inline(always)]
@@ -50,14 +50,62 @@ macro_rules! num_traits {
 };
 }
 
-num_traits!(Rgb, [r, g, b]);
-num_traits!(Bgr, [b, g, r]);
-num_traits!(Grb, [g, r, b]);
-num_traits!(Rgbw, [r, g, b, w]);
-num_traits!(Gray, [v]);
+macro_rules! num_traits_with_alpha {
+($name:ident, [$($bit:tt),*]) => {
+    impl<T: CheckedAdd, A: CheckedAdd> CheckedAdd for $name<T, A> {
+        #[inline(always)]
+        fn checked_add(&self, other: &Self) -> Option<Self> {
+            Some(Self {
+                $(
+                    $bit: self.$bit.checked_add(&other.$bit)?,
+                )+
+            })
+        }
+    }
 
-num_traits!(Rgba, [r, g, b, a]);
-num_traits!(Argb, [a, r, g, b]);
-num_traits!(Bgra, [b, g, r, a]);
-num_traits!(Abgr, [a, b, g, r]);
-num_traits!(GrayA, [v, a]);
+    impl<T: CheckedSub, A: CheckedSub> CheckedSub for $name<T, A> {
+        #[inline(always)]
+        fn checked_sub(&self, other: &Self) -> Option<Self> {
+            Some(Self {
+                $(
+                    $bit: self.$bit.checked_sub(&other.$bit)?,
+                )+
+            })
+        }
+    }
+
+    impl<T: CheckedMul, A: CheckedMul> CheckedMul for $name<T, A> {
+        #[inline(always)]
+        fn checked_mul(&self, other: &Self) -> Option<Self> {
+            Some(Self {
+                $(
+                    $bit: self.$bit.checked_mul(&other.$bit)?,
+                )+
+            })
+        }
+    }
+
+    impl<T: CheckedDiv, A: CheckedDiv> CheckedDiv for $name<T, A> {
+        #[inline(always)]
+        fn checked_div(&self, other: &Self) -> Option<Self> {
+            Some(Self {
+                $(
+                    $bit: self.$bit.checked_div(&other.$bit)?,
+                )+
+            })
+        }
+    }
+};
+}
+
+num_traits_without_alpha!(Rgb, [r, g, b]);
+num_traits_without_alpha!(Bgr, [b, g, r]);
+num_traits_without_alpha!(Grb, [g, r, b]);
+num_traits_without_alpha!(Rgbw, [r, g, b, w]);
+num_traits_without_alpha!(Gray, [v]);
+
+num_traits_with_alpha!(Rgba, [r, g, b, a]);
+num_traits_with_alpha!(Argb, [a, r, g, b]);
+num_traits_with_alpha!(Bgra, [b, g, r, a]);
+num_traits_with_alpha!(Abgr, [a, b, g, r]);
+num_traits_with_alpha!(GrayA, [v, a]);
