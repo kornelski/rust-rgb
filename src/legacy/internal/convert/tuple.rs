1cdd0c94425af7813dfc6e0d63512ec9d050c7ea
Kornel <kornel@geekhood.net>
2024-07-15 22:24:49 +0100

    Backport tuple impl from 0.9

--- src/legacy/internal/convert/tuple.rs
+++ src/legacy/internal/convert/tuple.rs
@@ -1,78 +1,82 @@
-use crate::alt::{BGR, BGRA};
-use crate::{RGB, RGBA};
 
-impl<T> From<(T, T, T)> for RGB<T> {
-    #[inline]
-    fn from(other: (T, T, T)) -> Self {
-        Self { r: other.0, g: other.1, b: other.2 }
-    }
-}
+use crate::formats::gray::Gray_v08;
+use crate::{Abgr, Argb, Bgr, Bgra, Grb, Rgb, Rgba, alt::GrayAlpha};
 
-impl<T> From<RGB<T>> for (T, T, T) {
-    #[inline]
-    fn from(value: RGB<T>) -> Self {
-        (value.r, value.g, value.b)
-    }
-}
+#[cfg(feature = "unstable-experimental")]
+use crate::GrayA;
+#[cfg(feature = "unstable-experimental")]
+use crate::formats::gray::Gray_v09;
 
-impl<T, A> From<(T, T, T, A)> for RGBA<T, A> {
-    #[inline]
-    fn from(other: (T, T, T, A)) -> Self {
-        Self {
-            r: other.0,
-            g: other.1,
-            b: other.2,
-            a: other.3,
+macro_rules! tuple_conversion {
+    ($name:ident, 1, [$($bit:tt:$num:tt),*]) => {
+        impl<R, S> From<$name<R>> for (S,) where R: Into<S> {
+            fn from(value: $name<R>) -> Self {
+                ($(value.$bit.into()),*,)
+            }
         }
-    }
-}
-
-impl<T, A> From<RGBA<T, A>> for (T, T, T, A) {
-    #[inline]
-    fn from(value: RGBA<T, A>) -> Self {
-        (value.r, value.g, value.b, value.a)
-    }
-}
-
-impl<T> From<(T, T, T)> for BGR<T> {
-    #[inline(always)]
-    fn from(other: (T, T, T)) -> Self {
-        Self { b: other.0, g: other.1, r: other.2 }
-    }
-}
-
-impl<T> From<BGR<T>> for (T, T, T) {
-    #[inline(always)]
-    fn from(value: BGR<T>) -> Self {
-        (value.b, value.g, value.r)
-    }
-}
-
-impl<T, A> From<(T, T, T, A)> for BGRA<T, A> {
-    #[inline(always)]
-    fn from(other: (T, T, T, A)) -> Self {
-        Self {
-            b: other.0,
-            g: other.1,
-            r: other.2,
-            a: other.3,
+    };
+    ($name:ident, 2, [$($bit:tt:$num:tt),*]) => {
+        impl<R, S> From<$name<R>> for (S, S) where R: Into<S> {
+            fn from(value: $name<R>) -> Self {
+                ($(value.$bit.into()),*)
+            }
         }
-    }
+        impl<R, S> From<(R, R)> for $name<S> where R: Into<S> {
+            fn from(value: (R, R)) -> Self {
+                Self{$($bit: value.$num.into()),*}
+            }
+        }
+    };
+    ($name:ident, 3, [$($bit:tt:$num:tt),*]) => {
+        impl<R, S> From<$name<R>> for (S, S, S) where R: Into<S> {
+            fn from(value: $name<R>) -> Self {
+                ($(value.$bit.into()),*)
+            }
+        }
+        impl<R, S> From<(R, R, R)> for $name<S> where R: Into<S> {
+            fn from(value: (R, R, R)) -> Self {
+                Self{$($bit: value.$num.into()),*}
+            }
+        }
+    };
+    ($name:ident, 4, [$($bit:tt:$num:tt),*]) => {
+        impl<R, S> From<$name<R>> for (S, S, S, S) where R: Into<S> {
+            fn from(value: $name<R>) -> Self {
+                ($(value.$bit.into()),*)
+            }
+        }
+        impl<R, S> From<(R, R, R, R)> for $name<S> where R: Into<S> {
+            fn from(value: (R, R, R, R)) -> Self {
+                Self{$($bit: value.$num.into()),*}
+            }
+        }
+    };
 }
 
-impl<T, A> From<BGRA<T, A>> for (T, T, T, A) {
-    #[inline(always)]
-    fn from(value: BGRA<T, A>) -> Self {
-        (value.b, value.g, value.r, value.a)
-    }
-}
+
+tuple_conversion!(Rgb, 3, [r:0, g:1, b:2]);
+tuple_conversion!(Bgr, 3, [b:0, g:1, r:2]);
+tuple_conversion!(Grb, 3, [g:0, r:1, b:2]);
+#[cfg(feature = "unstable-experimental")]
+tuple_conversion!(Gray_v09, 1, [v:0]);
+
+
+tuple_conversion!(Rgba, 4, [r:0, g:1, b:2, a:3]);
+tuple_conversion!(Argb, 4, [a:0, r:1, g:2, b:3]);
+tuple_conversion!(Bgra, 4, [b:0, g:1, r:2, a:3]);
+tuple_conversion!(Abgr, 4, [a:0, b:1, g:2, r:3]);
+#[cfg(feature = "unstable-experimental")]
+tuple_conversion!(GrayA, 2, [v:0, a:1]);
+
+tuple_conversion!(Gray_v08, 1, [0:0]);
+tuple_conversion!(GrayAlpha, 2, [0:0, 1:1]);
 
 #[test]
 fn converts() {
-    assert_eq!((1,2,3), RGB {r:1u8,g:2,b:3}.into());
-    assert_eq!(RGB {r:1u8,g:2,b:3}, (1,2,3).into());
-    assert_eq!((1,2,3,4), RGBA {r:1,g:2,b:3,a:4}.into());
-    assert_eq!(RGBA {r:1u8,g:2,b:3,a:4}, (1,2,3,4).into());
-    assert_eq!(BGRA {r:1u8,g:2,b:3,a:4}, (3,2,1,4).into());
-    assert_eq!(BGR {r:1u8,g:2,b:3}, (3,2,1).into());
+    assert_eq!((1,2,3), Rgb {r:1u8,g:2,b:3}.into());
+    assert_eq!(Rgb {r:1u8,g:2,b:3}, (1,2,3).into());
+    assert_eq!((1,2,3,4), Rgba {r:1,g:2,b:3,a:4}.into());
+    assert_eq!(Rgba {r:1u8,g:2,b:3,a:4}, (1,2,3,4).into());
+    assert_eq!(Bgra {r:1u8,g:2,b:3,a:4}, (3,2,1,4).into());
+    assert_eq!(Bgr {r:1u8,g:2,b:3}, (3,2,1).into());
 }
