a53411690b3dbdf6f5ff8f3f70d93ee3564720bc
Kornel <kornel@geekhood.net>
2024-08-04 18:45:44 +0100

    Add missing bytemuck

--- src/bytemuck_impl.rs
+++ src/bytemuck_impl.rs
@@ -3,6 +3,9 @@ use crate::formats::gray_a::GrayA;
 use crate::formats::gray_alpha::GrayAlpha_v08;
 use crate::formats::gray::Gray_v08;
 
+#[cfg(feature = "unstable-experimental")]
+use crate::formats::gray::Gray_v09;
+
 macro_rules! bytemuck {
     ($name:ident) => {
         unsafe impl<T: ::bytemuck::Zeroable> ::bytemuck::Zeroable for $name<T> {}
@@ -21,3 +24,6 @@ bytemuck!(GrayA);
 
 bytemuck!(GrayAlpha_v08);
 bytemuck!(Gray_v08);
+
+#[cfg(feature = "unstable-experimental")]
+bytemuck!(Gray_v09);
