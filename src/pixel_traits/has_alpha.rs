0fe2f616e2d323521092878927a8ff16c51c8ff0
ripytide <james.forsterer@gmail.com>
2024-08-04 11:47:29 +0100

    diambiguate `HasAlpha::alpha()` using fully qualified syntax

--- src/pixel_traits/has_alpha.rs
+++ src/pixel_traits/has_alpha.rs
@@ -8,17 +8,23 @@ use crate::{Abgr, Argb, Bgra, GrayA, Rgba};
 pub trait HasAlpha: HetPixel {
     /// Returns a copy of the pixel's alpha component.
     ///
+    /// Due to a naming conflict with several now-deprecated inherent
+    /// functions with the same name (such as `Rgb::alpha()`) the
+    /// `HasAlpha::alpha()` method requires fully qualified syntax for
+    /// disambiguation. The deprecated functions are due to be removed in a
+    /// future release which will solve this issue.
+    ///
     /// # Examples
     /// ```
     /// use rgb::{Rgba, HasAlpha};
     ///
     /// let mut rgba = Rgba {r: 0_u8, g: 10, b: 100, a: 50};
     ///
-    /// assert_eq!(rgba.alpha(), 50);
+    /// assert_eq!(HasAlpha::alpha(&rgba), 50);
     ///
     /// rgba.a = 0;
     ///
-    /// assert_eq!(rgba.alpha(), 0);
+    /// assert_eq!(HasAlpha::alpha(&rgba), 0);
     /// ```
     fn alpha(&self) -> Self::AlphaComponent;
     /// Returns a mutable borrow of the pixel's alpha component.
@@ -33,7 +39,7 @@ pub trait HasAlpha: HetPixel {
     ///
     /// *alpha += 100;
     ///
-    /// assert_eq!(rgba.alpha(), 150);
+    /// assert_eq!(HasAlpha::alpha(&rgba), 150);
     /// ```
     fn alpha_mut(&mut self) -> &mut Self::AlphaComponent;
 }
